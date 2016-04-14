1c03a99d849a0f82ec5419749d6f5486bb7a67ae
gkuanine <gkuanine@163.com>
2016-04-14 21:27:57 +0800

    `

--- app/src/main/java/com/yyeeo/test/MainActivity.java
+++ app/src/main/java/com/yyeeo/test/MainActivity.java
@@ -26,6 +26,15 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        FloatingActionButton fab2 = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
     }
 
     @Override
