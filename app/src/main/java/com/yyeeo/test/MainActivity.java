02cf2842d1bff521f2411cc416b65208f7e593c6
txfyteen <547369334@qq.com>
2016-04-14 21:54:10 +0800

    去掉不要的

--- app/src/main/java/com/yyeeo/test/MainActivity.java
+++ app/src/main/java/com/yyeeo/test/MainActivity.java
@@ -16,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
@@ -28,13 +29,7 @@ public class MainActivity extends AppCompatActivity {
         });
 
         FloatingActionButton fab2 = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
     }
 
 
